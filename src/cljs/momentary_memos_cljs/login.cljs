b3716ed7b7452d6767d31245a71ec5ba32f90bcf
Jeremy Berglund <jtberglund@gmail.com>
2018-03-04 17:04:51 -0800

    Small changes to login page

--- src/cljs/momentary_memos_cljs/login.cljs
+++ src/cljs/momentary_memos_cljs/login.cljs
@@ -9,15 +9,15 @@
         last-name (r/atom nil)]
 
     (fn [{:keys [on-submit]}]
-      [:div {:class "container"}
+      [:div {:class "container login"}
        [:h3 "Login"]
        [:form {:on-submit #(.preventDefault %)}
-        [:field-set
+        [:div {:class "login__form-container"}
          [text-input {:label "First Name"
                       :value @first-name
                       :on-change #(reset! first-name (-> % .-target .-value))}]
          [text-input {:label "Last Name"
                       :value @last-name
-                      :on-change #(reset! last-name (-> % .-target .-value))}]
-         [button {:text "Submit"
-                  :on-click #(on-submit {:first-name @first-name :last-name @last-name})}]]]])))
+                      :on-change #(reset! last-name (-> % .-target .-value))}]]
+        [button {:text "Submit"
+                 :on-click #(on-submit {:first-name @first-name :last-name @last-name})}]]])))
